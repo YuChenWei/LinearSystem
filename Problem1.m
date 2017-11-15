01e9d57c02eeb0f275667ad882aca66026232de4
YuChenWei <YuChenWei@users.noreply.github.com>
2017-11-15 09:29:14 +0800

    Add files via upload

--- Problem1.m
+++ Problem1.m
@@ -9,15 +9,19 @@ A=[0,1,0,0,0,0;
 B=[0 1 0 0 0 -1]';
 C=[0 0 1 0 0 0];
 D=0;
+c=eye(6);
 Sys=ss(A,B,C,D);
-[S,d]=eig(A)
+[S,d]=eig(A);
+sys=ss(A,B,c,D);
 % rank([-A B])
 % rank([i*eye(6)-A B])
 % rank([i*sqrt(3)*eye(6)-A B])
-for i=1:1:6
-    rank([eye(6)*d(i,i)-A,B])
-end
-
+Tf=10;
+Tm=5;
+T=0:0.01:Tf;
+Input(1,1:1:Tm/0.01+1)=0.698;
+Input(1,Tm/0.01+2:1:Tf/0.01+1)=0.302;
+lsim(sys,Input',T)
 % [Abar,Bbar,Cbar,T,k]=ctrbf(A,B,C);
 % TMP=ctrb(A,B);
 % Q=[TMP(:,1) TMP(:,2)];
